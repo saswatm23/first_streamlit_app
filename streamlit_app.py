f8828ba7dec2372f50cf5c40e55de79c9ca35322
Saswat Mishra <133883625+saswatm23@users.noreply.github.com>
2024-01-01 12:07:41 +0530

    Update streamlit_app.py
    Fetching the data in tabular format

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+
+# Normalize the data 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Show the data in tabular format
+streamlit.dataframe(fruityvice_normalized)
